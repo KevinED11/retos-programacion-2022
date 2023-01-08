0ed451a676b6fc447845910b0566f9da1ecd4a9d
KevinED11 <mendez_kael@hotmail.com>
2023-01-07 21:35:48 -0700

    cuarto reto

--- fibonacci-#2.py
+++ fibonacci-#2.py
@@ -7,13 +7,13 @@
 '''
 
 
-def fibonacci(num):
+def fibonacci(numbers):
 
     a, b = 0, 1
 
     fibonnaci_numbers = []
 
-    for number in range(num):
+    for number in range(numbers):
         fibonnaci_numbers.append(a)
 
         a, b = b, a + b
@@ -21,5 +21,22 @@ def fibonacci(num):
     return fibonnaci_numbers
 
 
+def fibonacci2(numbers):
+
+    fibonacci_numbers = [0, 1]
+
+    for i in range(2, numbers):
+
+        fibonacci_numbers.append(
+            fibonacci_numbers[i-1] + fibonacci_numbers[i-2])
+
+    return fibonacci_numbers
+
+
 if __name__ == '__main__':
+
     print(fibonacci(50))
+
+    print('\n')
+
+    print(fibonacci2(50))
---
+++ numero-primo-#3.py
@@ -0,0 +1,41 @@
+'''
+* Reto  # 3
+* ¿ES UN NÚMERO PRIMO?
+* Fecha publicación enunciado: 17/01/22
+* Fecha publicación resolución: 24/01/22
+* Dificultad: MEDIA
+*
+* Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
+* Hecho esto, imprime los números primos entre 1 y 100.
+'''
+
+
+def es_primo(number):
+
+    if number <= 1:
+        return False
+
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+
+        return True
+
+
+def numeros_primos():
+
+    numbers = list(range(1, 101))
+
+    primos = []
+
+    for num in numbers:
+        if es_primo(num):
+            primos.append(num)
+
+    return primos
+
+
+if __name__ == '__main__':
+
+    print(numeros_primos())
