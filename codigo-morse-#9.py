54d2dd48438a3b2efdb9c9cc536eb972d9b552b4
KevinED11 <mendez_kael@hotmail.com>
2023-01-12 11:22:06 -0700

    reto #10

---
+++ codigo-morse-#9.py
@@ -0,0 +1,64 @@
+
+'''
+ Reto #9
+ * CÓDIGO MORSE
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que sea capaz de transformar texto natural a código morse y viceversa.
+ * - Debe detectar automáticamente de qué tipo se trata y realizar la conversión.
+'''
+
+
+morse_code_dict = {
+
+    'A': '.-', 'B': '-...',
+    'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-',
+    'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-'
+}
+
+
+def text_to_morse(text):
+
+    result = ''
+
+    for let in text.upper():
+
+        if let == " ":
+            result += " "
+
+        else:
+            result += morse_code_dict[let] + " "
+
+    return result
+
+
+def morse_to_text(morse):
+    morse_code_dict_inverse = {v: k for k, v in morse_code_dict.items()}
+    morse_text = ''
+
+    words = morse.split()
+
+    for let in words:
+        if let in morse_code_dict_inverse:
+            morse_text += morse_code_dict_inverse[let]
+
+    return morse_text.lower()
+
+
+if __name__ == '__main__':
+
+    print(text_to_morse('kevin'))
+
+    print(morse_to_text('-.- . ...- .. -. '))
