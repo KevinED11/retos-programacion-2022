8a3c2f18cbed9cb7ea6e2d2e5e885893a0d742d4
KevinED11 <mendez_kael@hotmail.com>
2023-01-22 22:02:39 -0700

    reto 20 completado

--- conversor-tiempo.py
+++ conversor-tiempo.py
@@ -6,15 +6,11 @@
 
 def time_conversor(days: int, hours: int, minutes: int, seconds: int) -> str:
 
-    try:
-        if any(valor <= 0 for valor in [days, hours, minutes, seconds]):
-            raise ValueError(
-                'Los valores deben ser positivos, evita poner 0 o un numero negativo')
+    if any(valor <= 0 for valor in [days, hours, minutes, seconds]):
+        raise ValueError(
+            'Los valores deben ser positivos, evita poner 0 o un numero negativo')
 
-        return (days * 86400000) + (hours * 3600000) + (minutes * 60000) + (seconds * 1000)
-
-    except TypeError as error:
-        return error
+    return (days * 86400000) + (hours * 3600000) + (minutes * 60000) + (seconds * 1000)
 
 
 if __name__ == '__main__':
