70dac04960772ec737167815c58c290e008e88b7
KevinED11 <mendez_kael@hotmail.com>
2023-01-23 11:50:23 -0700

    terminado

---
+++ conjuntos.py
@@ -0,0 +1,30 @@
+'''
+ * Crea una función que reciba dos array, un booleano y retorne un array.
+ * - Si el booleano es verdadero buscará y retornará los elementos comunes
+ *   de los dos array.
+ * - Si el booleano es falso buscará y retornará los elementos no comunes
+ *   de los dos array.
+ * - No se pueden utilizar operaciones del lenguaje que
+ *   lo resuelvan directamente.
+ '''
+
+def conjuntos(array1: list, array2: list, booleano: bool) -> list:
+
+    elementos = []
+    if not booleano:
+        for i in array1:
+            if i not in array2:
+                elementos.append(i)
+        for i in array2:
+            if i not in array1:
+                elementos.append(i)
+
+    else:
+        for i in array1:
+            if i in array2:
+                elementos.append(i)
+
+    return elementos
+
+if __name__ == '__main__':
+    print(conjuntos([1, 2, 3, 4, 7], [1, 2, 7], True))
