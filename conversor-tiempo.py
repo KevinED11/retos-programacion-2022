bdc7a4cea7a9bcf32af22080c1e6f55ef84b6bbf
KevinED11 <mendez_kael@hotmail.com>
2023-01-22 21:53:04 -0700

    reto 20 completado

---
+++ conversor-tiempo.py
@@ -0,0 +1,22 @@
+'''
+* Crea una función que reciba días, horas, minutos y segundos(como enteros)
+* y retorne su resultado en milisegundos.
+'''
+
+
+def time_conversor(days: int, hours: int, minutes: int, seconds: int) -> str:
+
+    try:
+        if any(valor <= 0 for valor in [days, hours, minutes, seconds]):
+            raise ValueError(
+                'Los valores deben ser positivos, evita poner 0 o un numero negativo')
+
+        return (days * 86400000) + (hours * 3600000) + (minutes * 60000) + (seconds * 1000)
+
+    except TypeError as error:
+        return error
+
+
+if __name__ == '__main__':
+    result = time_conversor(1, 1, 1, 1)
+    print(result)
