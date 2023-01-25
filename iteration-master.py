e1834c06d7f97a3ec7874ca04f38060c3af54dc4
KevinED11 <mendez_kael@hotmail.com>
2023-01-25 11:36:26 -0700

    forma 1

---
+++ iteration-master.py
@@ -0,0 +1,11 @@
+'''
+ * Quiero contar del 1 al 100 de uno en uno (imprimiendo cada uno).
+ * ¿De cuántas maneras eres capaz de hacerlo?
+ * Crea el código para cada una de ellas.
+ '''
+
+if __name__ == '__main__':
+    for num in range(1, 100 + 1):
+        print(num)
+
+
